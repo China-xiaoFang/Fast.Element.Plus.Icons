317acb768b52840b26c8438dc6f006aa23b90abe
小方 <2875616188@qq.com>
2022-07-02 14:08:48 +0800

    添加Base Model

---
+++ src/Hello.NET.Core/BaseFactory/Const/ClaimConst.cs
@@ -0,0 +1,54 @@
+﻿namespace Hello.NET.Core.BaseFactory.Const;
+
+public class ClaimConst
+{
+    /// <summary>
+    /// 用户Id
+    /// </summary>
+    public const string CLAINM_USERID = "UserId";
+
+    /// <summary>
+    /// 账号
+    /// </summary>
+    public const string CLAINM_ACCOUNT = "Account";
+
+    /// <summary>
+    /// 名称
+    /// </summary>
+    public const string CLAINM_NAME = "Name";
+
+    /// <summary>
+    /// 是否超级管理
+    /// </summary>
+    public const string CLAINM_SUPERADMIN = "SuperAdmin";
+
+    /// <summary>
+    /// 租户Id
+    /// </summary>
+    public const string TENANT_ID = "TenantId";
+
+    /// <summary>
+    /// 租户名称
+    /// </summary>
+    public const string TENANT_NAME = "TenantName";
+
+    /// <summary>
+    /// 租户密钥
+    /// </summary>
+    public const string TENANT_SECRET = "TenantSecret";
+
+    /// <summary>
+    /// 默认系统管理员
+    /// </summary>
+    public const string ROLE_DEFAULT_SYSTEM_ADMIN = "系统管理员";
+
+    /// <summary>
+    /// 默认租户管理员
+    /// </summary>
+    public const string ROLE_DEFAULT_TENANT_ADMIN = "租户管理员";
+
+    /// <summary>
+    /// 删除字段
+    /// </summary>
+    public const string DELETE_FIELD = "IsDeleted";
+}
---
+++ src/Hello.NET.Core/BaseFactory/PrimaryKeyEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Hello.NET.Core.BaseFactory;
+
+/// <summary>
+/// 主键实体基类
+/// </summary>
+public abstract class PrimaryKeyEntity
+{
+    /// <summary>
+    /// 主键Id
+    /// </summary>
+    [SugarColumn(ColumnDescription = "Id主键", IsPrimaryKey = true)]
+    // 注意是在这里定义你的公共实体
+    public virtual long Id { get; set; }
+}
