5a5caa34a19d4f31d0b7d1be081774dc5f1ec04b
小方 <2875616188@qq.com>
2022-08-18 16:13:30 +0800

    更改项目结构

--- src/Fast.NET.Core/AttributeFilter/WeChatPayCallbackOpLog.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fast.NET.Core.AttributeFilter;
-
-/// <summary>
-/// 微信支付回调操作日志
-/// </summary>
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property)]
-public class WeChatPayCallbackOpLog : Attribute
-{
-}
