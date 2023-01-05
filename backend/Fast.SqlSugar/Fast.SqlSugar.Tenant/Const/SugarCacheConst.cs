23dabe8ff22176e9095d8978caefa4fdbad5a760
小方 <2875616188@qq.com>
2023-01-05 23:18:25 +0800

    添加租户版本的SqlSugar扩展，仓储扩展方法待优化

---
+++ backend/Fast.SqlSugar/Fast.SqlSugar.Tenant/Const/SugarCacheConst.cs
@@ -0,0 +1,12 @@
+﻿namespace Fast.SqlSugar.Tenant.Const;
+
+/// <summary>
+/// Sugar缓存常量
+/// </summary>
+internal class SugarCacheConst
+{
+    /// <summary>
+    /// 数据库信息缓存
+    /// </summary>
+    public const string TenantDbInfo = "Tenant:DbInfo:";
+}
---
+++ backend/Fast.SqlSugar/Fast.SqlSugar.Tenant/Internal/Enum/SugarDbTypeEnum.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+
+namespace Fast.SqlSugar.Tenant.Internal.Enum;
+
+/// <summary>
+/// Db类型枚举父类
+/// </summary>
+public enum SugarDbTypeEnum
+{
+    /// <summary>
+    /// 租户库
+    /// </summary>
+    [Description("租户库")]
+    Tenant = 0,
+
+    /// <summary>
+    /// 默认库
+    /// </summary>
+    [Description("默认库")]
+    Default = 1,
+
+    /// <summary>
+    /// Admin库
+    /// </summary>
+    [Description("Admin库")]
+    Admin = 2,
+
+    /// <summary>
+    /// User库
+    /// </summary>
+    [Description("User库")]
+    User = 6,
+
+    /// <summary>
+    /// System库
+    /// </summary>
+    [Description("System库")]
+    System = 11,
+
+    /// <summary>
+    /// Log库
+    /// </summary>
+    [Description("Log库")]
+    Log = 22,
+
+    /// <summary>
+    /// Config库
+    /// </summary>
+    [Description("Config库")]
+    Config = 33,
+
+    /// <summary>
+    /// Data库
+    /// </summary>
+    [Description("Data库")]
+    Data = 44,
+
+    /// <summary>
+    /// Other库
+    /// </summary>
+    [Description("Other库")]
+    Other = 99,
+}
