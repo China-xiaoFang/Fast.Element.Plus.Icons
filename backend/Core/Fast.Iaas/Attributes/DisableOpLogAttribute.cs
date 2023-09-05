1152222aa5a172b981dd03ee400e64f69ad9683d
小方 <2875616188@qq.com>
2023-09-05 20:26:22 +0800

    删除所有代码进行重构

--- backend/Core/Fast.Iaas/Attributes/DisableOpLogAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fast.Iaas.Attributes;
-
-/// <summary>
-/// 禁用操作日志
-/// </summary>
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property)]
-public class DisableOpLogAttribute : Attribute
-{
-}
