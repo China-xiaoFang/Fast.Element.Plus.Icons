9f5f519ddf9384a199a16fb0da1632bd2b0f7249
小方 <2875616188@qq.com>
2023-10-03 21:03:54 +0800

    初始化网关项目

--- backend/Fast.MicroGateway/Fast.MicroGateway.Api/Program.cs
+++ backend/Fast.MicroGateway/Fast.MicroGateway.Api/Program.cs
@@ -1,91 +1,19 @@
-using Fast.Cache.Extensions;
-using Fast.Core;
-using Fast.Core.Extensions;
-using Fast.CorsAccessor.Extensions;
-using Fast.DataValidation.Extensions;
-using Fast.DynamicApplication.Extensions;
-using Fast.EventBus.Extensions;
-using Fast.Exception.Extensions;
-using Fast.Logging.Extensions;
-using Fast.Test.Api;
-using Fast.UnifyResult.Extensions;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using RabbitMQ.Client;
 
-var builder = WebApplication.CreateBuilder(args).Initialize();
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-
-// Customize the console log output template.
-builder.Logging.AddConsoleFormatter(options => { options.DateFormat = "yyyy-MM-dd HH:mm:ss"; });
-
-builder.Services.AddLogging();
-
-builder.Services.AddControllers();
-
-//builder.Services.AddSpecificationDocuments();
-
-builder.Services.AddDynamicApiControllers();
-
-builder.Services.AddDataValidation();
-
-builder.Services.AddFriendlyException();
-
-builder.Services.AddUnifyResult<RESTfulResultProvider>();
-
-builder.Services.AddCache();
-
 builder.Services.AddOcelot();
-
-//builder.Services.AddSqlSugar();
-
-// Add event bus.
-//builder.Services.AddEventBus(options =>
-//{
-//    // 创建连接工厂
-//    var factory = App.GetConfig<ConnectionFactory>("RabbitMQConnection");
-
-//    // 创建默认内存通道事件源对象
-//    var mqEventSourceStorer = new RabbitMQEventSourceStorer(factory, "WMS.Event.Bus", 3000);
-
-//    // 替换默认事件总线存储器
-//    options.ReplaceStorer(serviceProvider => mqEventSourceStorer);
-
-//    // 注册事件总线重试服务
-//    options.AddFallbackPolicy<EventFallbackPolicy>();
-//});
-//builder.Services.AddEventBus();
-
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Mandatory Https.
+// Configure the HTTP request pipeline.
+
 app.UseHttpsRedirection();
 
-// Enable compression.
-//app.UseResponseCompression();
-
-// Add the status code interception middleware.
-app.UseUnifyResultStatusCodes();
-
-// Add cross-domain middleware.
-app.UseCorsAccessor();
-
-app.UseStaticFiles();
-
-app.UseOcelot();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseRouting();
-
-app.MapControllers();
+app.UseOcelot().Wait();
 
 app.Run();
