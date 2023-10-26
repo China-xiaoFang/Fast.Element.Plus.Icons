927c5ffbacd39dc9752bf2218f6838a26a6c7b9a
小方 <2875616188@qq.com>
2023-10-26 16:56:22 +0800

    玩脱了，炸了，哈哈哈

---
+++ backend/Fast.Test/Fast.Test.Api/Services/TestService.cs
@@ -0,0 +1,15 @@
+﻿using Fast.DependencyInjection;
+
+namespace Fast.Test.Api.Services;
+
+public class TestService : ITestService, ITransientDependency
+{
+    /// <summary>
+    /// 测试
+    /// </summary>
+    /// <returns></returns>
+    public string Test()
+    {
+        return "Hello World!";
+    }
+}
