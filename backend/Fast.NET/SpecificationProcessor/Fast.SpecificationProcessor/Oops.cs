f7230cddecb770577696490813d0fb34f3f4b649
小方 <2875616188@qq.com>
2023-10-28 11:21:26 +0800

    注释了 Oops

--- backend/Fast.NET/SpecificationProcessor/Fast.SpecificationProcessor/Oops.cs
+++ backend/Fast.NET/SpecificationProcessor/Fast.SpecificationProcessor/Oops.cs
@@ -17,60 +17,60 @@ using Fast.NET;
 // ReSharper disable once CheckNamespace
 namespace Fast.SpecificationProcessor;
 
-/// <summary>
-/// 抛异常静态类
-/// </summary>
-public static class Oops
-{
-    /// <summary>
-    /// 抛出业务异常信息
-    /// </summary>
-    /// <param name="errorMessage"><see cref="string"/>异常消息</param>
-    /// <param name="args">String.Format 参数</param>
-    /// <returns><see cref="UserFriendlyException"/>异常实例</returns>
-    [Obsolete("This method is deprecated, use throw new UserFriendlyException() instead.")]
-    public static UserFriendlyException Bah(string errorMessage, params object[] args)
-    {
-        return new UserFriendlyException(string.Format(errorMessage, args));
-    }
+///// <summary>
+///// 抛异常静态类
+///// </summary>
+//public static class Oops
+//{
+//    /// <summary>
+//    /// 抛出业务异常信息
+//    /// </summary>
+//    /// <param name="errorMessage"><see cref="string"/>异常消息</param>
+//    /// <param name="args">String.Format 参数</param>
+//    /// <returns><see cref="UserFriendlyException"/>异常实例</returns>
+//    [Obsolete("This method is deprecated, use throw new UserFriendlyException() instead.")]
+//    public static UserFriendlyException Bah(string errorMessage, params object[] args)
+//    {
+//        return new UserFriendlyException(string.Format(errorMessage, args));
+//    }
 
-    /// <summary>
-    /// 抛出字符串异常
-    /// </summary>
-    /// <param name="errorMessage"><see cref="string"/>异常消息</param>
-    /// <param name="args">String.Format 参数</param>
-    /// <returns><see cref="Exception"/>异常实例</returns>
-    [Obsolete("This method is deprecated, use throw new Exception() instead.")]
-    public static Exception Oh(string errorMessage, params object[] args)
-    {
-        return new Exception(string.Format(errorMessage, args));
-    }
+//    /// <summary>
+//    /// 抛出字符串异常
+//    /// </summary>
+//    /// <param name="errorMessage"><see cref="string"/>异常消息</param>
+//    /// <param name="args">String.Format 参数</param>
+//    /// <returns><see cref="Exception"/>异常实例</returns>
+//    [Obsolete("This method is deprecated, use throw new Exception() instead.")]
+//    public static Exception Oh(string errorMessage, params object[] args)
+//    {
+//        return new Exception(string.Format(errorMessage, args));
+//    }
 
-    /// <summary>
-    /// 抛出字符串异常
-    /// </summary>
-    /// <param name="errorMessage"><see cref="string"/>异常消息</param>
-    /// <param name="exceptionType">具体异常类型</param>
-    /// <param name="args">String.Format 参数</param>
-    /// <returns><see cref="Exception"/>异常实例</returns>
-    [Obsolete("This method is deprecated, use throw new Exception() instead.")]
-    public static Exception Oh(string errorMessage, Type exceptionType, params object[] args)
-    {
-        var exceptionMessage = string.Format(errorMessage, args);
-        return new Exception(exceptionMessage,
-            innerException: Activator.CreateInstance(exceptionType, exceptionMessage) as Exception);
-    }
+//    /// <summary>
+//    /// 抛出字符串异常
+//    /// </summary>
+//    /// <param name="errorMessage"><see cref="string"/>异常消息</param>
+//    /// <param name="exceptionType">具体异常类型</param>
+//    /// <param name="args">String.Format 参数</param>
+//    /// <returns><see cref="Exception"/>异常实例</returns>
+//    [Obsolete("This method is deprecated, use throw new Exception() instead.")]
+//    public static Exception Oh(string errorMessage, Type exceptionType, params object[] args)
+//    {
+//        var exceptionMessage = string.Format(errorMessage, args);
+//        return new Exception(exceptionMessage,
+//            innerException: Activator.CreateInstance(exceptionType, exceptionMessage) as Exception);
+//    }
 
-    /// <summary>
-    /// 抛出字符串异常
-    /// </summary>
-    /// <typeparam name="TException">具体异常类型</typeparam>
-    /// <param name="errorMessage"><see cref="string"/>异常消息</param>
-    /// <param name="args">String.Format 参数</param>
-    /// <returns><see cref="Exception"/>异常实例</returns>
-    [Obsolete("This method is deprecated, use throw new Exception() instead.")]
-    public static Exception Oh<TException>(string errorMessage, params object[] args) where TException : class
-    {
-        return Oh(errorMessage, typeof(TException), args);
-    }
-}
+//    /// <summary>
+//    /// 抛出字符串异常
+//    /// </summary>
+//    /// <typeparam name="TException">具体异常类型</typeparam>
+//    /// <param name="errorMessage"><see cref="string"/>异常消息</param>
+//    /// <param name="args">String.Format 参数</param>
+//    /// <returns><see cref="Exception"/>异常实例</returns>
+//    [Obsolete("This method is deprecated, use throw new Exception() instead.")]
+//    public static Exception Oh<TException>(string errorMessage, params object[] args) where TException : class
+//    {
+//        return Oh(errorMessage, typeof(TException), args);
+//    }
+//}
