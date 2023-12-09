b4d2b851a542c121b466f74142d9f0e278515e8c
小方 <2875616188@qq.com>
2023-12-09 13:56:20 +0800

    前端模板图标支持 svg

---
+++ frontend-template/src/plugins/icon/index.ts
@@ -0,0 +1,16 @@
+import { type App } from "vue";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import FIcon from "@/components/FIcon/index.vue";
+
+export function loadIcon(app: App) {
+    /**
+     * 全局注册 FIcon
+     * 使用方法：<FIcon name="name" size="size" color="color">
+     */
+    app.component("FIcon", FIcon);
+
+    /** 注册所有 Element Plus Icon */
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+        app.component(key, component);
+    }
+}
--- frontend-template/src/plugins/index.ts
+++ frontend-template/src/plugins/index.ts
@@ -1,6 +1,8 @@
 import { type App } from "vue";
 import { loadElementPlus } from "./element-plus";
+import { loadIcon } from "./icon";
 
 export function loadPlugins(app: App) {
     loadElementPlus(app);
+    loadIcon(app);
 }
