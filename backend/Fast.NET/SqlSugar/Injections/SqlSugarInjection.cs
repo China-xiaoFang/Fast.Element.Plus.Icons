061f9604432ef65773383d548c9ac0ae09b76378
小方 <2875616188@qq.com>
2024-01-20 11:03:38 +0800

    修复个小Bug，发布 `v3.4.17.3` 版本

--- backend/Fast.NET/SqlSugar/Injections/SqlSugarInjection.cs
+++ backend/Fast.NET/SqlSugar/Injections/SqlSugarInjection.cs
@@ -65,8 +65,6 @@ public class SqlSugarInjection : IHostInjection
 
             SqlSugarContext.DefaultConnectionConfig = SqlSugarContext.GetConnectionConfig(SqlSugarContext.ConnectionSettings);
 
-            ISqlSugarEntityHandler sqlSugarEntityHandler = null;
-
             // 查找Sugar实体处理程序提供者
             var iSqlSugarEntityHandlerType =
                 IaaSContext.EffectiveTypes.FirstOrDefault(f =>
@@ -75,16 +73,14 @@ public class SqlSugarInjection : IHostInjection
             {
                 // 注册Sugar实体处理程序
                 services.AddScoped(typeof(ISqlSugarEntityHandler), iSqlSugarEntityHandlerType);
-
-                var serviceProvider = services.BuildServiceProvider();
-                IaaSContext.UnmanagedObjects.Add(serviceProvider);
-                // 获取 Sugar实体处理 接口的实现类
-                sqlSugarEntityHandler = serviceProvider.GetRequiredService<ISqlSugarEntityHandler>();
             }
 
             // 注册 SqlSugarClient，这里注册一遍是因为防止直接使用 ISqlSugarClient
             services.AddScoped<ISqlSugarClient>(serviceProvider =>
             {
+                // 获取 Sugar实体处理 接口的实现类
+                var sqlSugarEntityHandler = serviceProvider.GetRequiredService<ISqlSugarEntityHandler>();
+
                 var sqlSugarClient = new SqlSugarClient(SqlSugarContext.DefaultConnectionConfig);
 
                 // 执行超时时间
