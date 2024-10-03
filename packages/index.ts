8f7be5e6531d1c8b3e5faa0fd34001a19ce76b83
小方 <2875616188@qq.com>
2024-10-03 13:47:59 +0800

    导出 INSTALLED_KEY

--- packages/index.ts
+++ packages/index.ts
@@ -1,6 +1,8 @@
-import { makeInstaller } from "@icons-vue/build";
+import { INSTALLED_KEY, makeInstaller } from "@icons-vue/build";
 
 export * from "@icons-vue/icons";
+
+export { INSTALLED_KEY };
 
 const installer = makeInstaller();
 
