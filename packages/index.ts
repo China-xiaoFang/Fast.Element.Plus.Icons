a1ad5fee72d5c3a8f728d0cfb6fa1b4a3f7a0f38
小方 <2875616188@qq.com>
2024-10-09 10:41:17 +0800

    1

--- packages/index.ts
+++ packages/index.ts
@@ -1,7 +1,7 @@
 import { makeInstaller } from "./make-installer";
+
 export { INSTALLED_KEY } from "./make-installer";
 export * as FastElementPlusIconsVue from "@icons-vue/icons";
-
 export * from "@icons-vue/icons";
 
 const installer = makeInstaller();
